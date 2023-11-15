4dfa5f01e0b492c0bbba92ff1ea10fd37ed5476b
Peter Osusky <peterosusky94@gmail.com>
2023-11-15 14:01:40 +0100

    implemented CardView

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -5,8 +5,8 @@
 //  Created by Peter on 15/11/2023.
 //
 
-import Foundation
 import SwiftUI
+
 
 enum Theme: String {
     case bubblegum
@@ -28,16 +28,11 @@ enum Theme: String {
     
     var accentColor: Color {
         switch self {
-        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow:
-            return .black
-        case .indigo, .magenta, .navy, .oxblood, .purple:
-            return .white
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
-    
     var mainColor: Color {
         Color(rawValue)
     }
 }
-
-
---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,8 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Peter on 15/11/2023.
+//
+
+import Foundation
