7bbe619bd2d50ebc28aa9c65fac2b66426cf60f4
Peter Osusky <peterosusky94@gmail.com>
2023-11-15 15:01:27 +0100

    added navigation, title and toolbar

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Peter on 15/11/2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
