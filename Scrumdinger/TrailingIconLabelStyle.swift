87889dd564c6a22b96ecdd41cee576488c24f675
Peter Osusky <peterosusky94@gmail.com>
2023-11-15 14:35:22 +0100

    added scrums list

--- Scrumdinger/TrailingIconLabelStyle.swift
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -5,4 +5,17 @@
 //  Created by Peter on 15/11/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
