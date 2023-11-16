8d5c99202e990074d8dc0120632992b739a0debe
Peter Osusky <peterosusky94@gmail.com>
2023-11-16 12:21:57 +0100

    implemented DetailEditView

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -8,18 +8,40 @@
 import SwiftUI
 
 struct DetailEditView: View {
-    @State private var emptyScrum = DailyScrum.emptyScrum
+    @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
     
     var body: some View {
         Form {
             Section(header: Text("Meeting Info")) {
-                TextField("Title", text: $emptyScrum.title)
+                TextField("Title", text: $scrum.title)
                 HStack {
-                    Slider(value: $emptyScrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
                         Text("Length")
                     }
                     Spacer()
-                    Text("\(emptyScrum.lengthInMinutes) minutes")
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach (scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete(perform: { indexSet in
+                    scrum.attendees.remove(atOffsets: indexSet)
+                })
+                HStack {
+                    TextField("New attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
                 }
             }
         }
--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
+    @State private var isPresentingEditView = false
     
     var body: some View {
         List {
@@ -39,7 +40,30 @@ struct DetailView: View {
                     Label(attendee.name, systemImage: "person")
                 }
             }
-            .navigationTitle(scrum.title)
+        }
+        .navigationTitle(scrum.title)
+        .toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationStack {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
         }
     }
 }
